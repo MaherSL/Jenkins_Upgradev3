dd9625f7c296e6a981fcca40ae20a04a0b39160d
MaherSL <maher.slama@yahoo.fr>
2021-05-31 16:34:45 +0100

    test commit with errors

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -32,8 +32,8 @@ public class TestGreeter {
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessage2() {
-    String someone = "World, Thanks  you making me Happy ";
+  public void greetShouldIncludeGreetingPhrase() {
+    String someone = "World, Thanks you making me Happy ";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
